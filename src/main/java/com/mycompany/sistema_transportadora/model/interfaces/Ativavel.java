9b440d8e1c041093f73798b71c9696fa78aec924
victor-kohler <victorkohler2016@gmail.com>
2025-05-30 16:56:52 -0300

    Documentação Ativavel.java

--- src/main/java/com/mycompany/sistema_transportadora/model/interfaces/Ativavel.java
+++ src/main/java/com/mycompany/sistema_transportadora/model/interfaces/Ativavel.java
@@ -1,6 +1,9 @@
 package com.mycompany.sistema_transportadora.model.interfaces;
 
+// Interface que define o comportamento de ativação e desativação de uma entidade.
+// Classes que implementam essa interface devem informar se estão ativas e permitir a desativação.
+
 public interface Ativavel {
-    boolean isAtivo();
-    void desativar();
+    boolean isAtivo(); // Verifica se a entidade está ativa.
+    void desativar(); // Desativa a entidade.
 }
