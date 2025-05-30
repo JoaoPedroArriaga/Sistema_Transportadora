81b5c8ed62cfa9a832aa25d1c3a39d1bff4a86f4
victor-kohler <victorkohler2016@gmail.com>
2025-05-30 16:35:24 -0300

    Documentação StatusCarga.java

--- src/main/java/com/mycompany/sistema_transportadora/model/enums/StatusCarga.java
+++ src/main/java/com/mycompany/sistema_transportadora/model/enums/StatusCarga.java
@@ -1,15 +1,17 @@
+// Enumeração que representa os possíveis status de uma carga no sistema de transportadora.
+
 package com.mycompany.sistema_transportadora.model.enums;
 
 public enum StatusCarga {
-    PENDENTE,
-    ARMAZENADA,
-    EM_TRANSPORTE,
-    ENTREGUE,
-    EXTRAVIADA,
-    CANCELADA;
-
+    PENDENTE, // Indica que a carga foi registrada, mas ainda não está pronta para transporte.
+    ARMAZENADA, // Indica que a carga está armazenada em algum centro de distribuição ou depósito.
+    EM_TRANSPORTE, // Indica que a carga está em deslocamento, ou seja, em trânsito até o destino.
+    ENTREGUE, // Indica que a carga chegou corretamente ao destinatário final.
+    EXTRAVIADA, // Indica que a carga foi extraviada, ou seja, perdida ou não localizada.
+    CANCELADA; // Indica que a operação de transporte da carga foi cancelada.
+ 
     @Override
     public String toString() {
-        return this.name().charAt(0) + this.name().substring(1).toLowerCase();
+        return this.name().charAt(0) + this.name().substring(1).toLowerCase(); //  Retorna uma representação legível do nome do status.
     }
 }
